3c673c5a558f27b838dbbb90ddc63075cff8f676
Ahmzan <git@ahmzan.net>
2025-06-05 23:37:48 +0700

    feat: implement read article

--- src/components/Artikel.tsx
+++ src/components/Artikel.tsx
@@ -1,70 +1,57 @@
+import { database } from '@/lib/firebase'
+import { type ArtikelData } from '@/pages/admin/artikel'
+import { onValue, ref } from 'firebase/database'
+import { useEffect, useState } from 'react'
+
 const Artikel = () => {
+  const [artikels, setArtikels] = useState<ArtikelData[]>([])
+
+  useEffect(() => {
+    const refArtikels = ref(database, 'artikels')
+
+    const unsub = onValue(refArtikels, snapArtikels => {
+      const dataArtikels: ArtikelData[] = []
+
+      snapArtikels.forEach(snapArtikel => {
+        const data = snapArtikel.val() as ArtikelData
+        dataArtikels.push({ ...data, key: snapArtikel.key })
+      })
+
+      setArtikels(dataArtikels)
+      console.log(dataArtikels)
+    })
+
+    return () => unsub()
+  }, [])
+
   return (
     <section id='artikel' className='py-20 container mx-auto px-6'>
       <h3 className='text-3xl font-bold mb-12 text-center' data-aos='fade-up'>
         Artikel Kesehatan
       </h3>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-        <div
-          className='bg-white shadow-lg rounded-lg overflow-hidden card-hover'
-          data-aos='fade-up'
-          data-aos-delay='100'
-        >
-          <img
-            src='https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            alt='Olahraga'
-            className='w-full h-48 object-cover'
-          />
-          <div className='p-6'>
-            <h4 className='font-semibold text-xl mb-3'>Manfaat Olahraga Rutin</h4>
-            <p className='text-gray-600'>
-              Olahraga membantu menjaga kebugaran, meningkatkan mood, dan mencegah penyakit...
-            </p>
-            <a href='#' className='text-blue-800 font-semibold mt-4 inline-block hover:underline'>
-              Baca Selengkapnya →
-            </a>
+        {artikels.map(artikel => (
+          <div
+            key={artikel.id}
+            className='bg-white shadow-lg rounded-lg overflow-hidden card-hover'
+            data-aos='fade-up'
+            data-aos-delay='300'
+          >
+            <img src={artikel.imageUrl} alt={artikel.title} className='w-full h-48 object-cover' />
+            <div className='p-6'>
+              <h4 className='font-semibold text-xl mb-3'>{artikel.title}</h4>
+              <p className='text-gray-600'>
+                {artikel.content.length > 70 ? artikel.content.substring(0, 70) + '...' : artikel.content}
+              </p>
+              <a
+                href={'/artikel/' + artikel.key}
+                className='text-blue-800 font-semibold mt-4 inline-block hover:underline'
+              >
+                Baca Selengkapnya →
+              </a>
+            </div>
           </div>
-        </div>
-        <div
-          className='bg-white shadow-lg rounded-lg overflow-hidden card-hover'
-          data-aos='fade-up'
-          data-aos-delay='200'
-        >
-          <img
-            src='https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            alt='Makanan Sehat'
-            className='w-full h-48 object-cover'
-          />
-          <div className='p-6'>
-            <h4 className='font-semibold text-xl mb-3'>Makanan Sehat untuk Jantung</h4>
-            <p className='text-gray-600'>
-              Mengonsumsi sayur, buah, dan biji-bijian baik untuk menjaga kesehatan jantung...
-            </p>
-            <a href='#' className='text-blue-800 font-semibold mt-4 inline-block hover:underline'>
-              Baca Selengkapnya →
-            </a>
-          </div>
-        </div>
-        <div
-          className='bg-white shadow-lg rounded-lg overflow-hidden card-hover'
-          data-aos='fade-up'
-          data-aos-delay='300'
-        >
-          <img
-            src='https://images.unsplash.com/photo-1511295742362-92c96b1cf484?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            alt='Tips Tidur Berkualitas'
-            className='w-full h-48 object-cover'
-          />
-          <div className='p-6'>
-            <h4 className='font-semibold text-xl mb-3'>Tips Tidur Berkualitas</h4>
-            <p className='text-gray-600'>
-              Tidur cukup dan berkualitas sangat penting untuk kesehatan mental dan fisik...
-            </p>
-            <a href='#' className='text-blue-800 font-semibold mt-4 inline-block hover:underline'>
-              Baca Selengkapnya →
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   )
---
+++ src/pages/artikel/[key].tsx
@@ -0,0 +1,49 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import { database } from '@/lib/firebase'
+import { onValue, ref } from 'firebase/database'
+import { useEffect, useState } from 'react'
+
+import { useParams } from 'react-router'
+import type { ArtikelData } from '../admin/artikel'
+
+export default function ArtikelPage() {
+  const params = useParams()
+
+  const [title, setTitle] = useState<string>()
+  const [content, setContent] = useState<string>()
+  const [imageUrl, setImageUrl] = useState<string>()
+
+  useEffect(() => {
+    const key = params.key
+    if (!key) return
+
+    const refArtikel = ref(database, 'artikels/' + key)
+
+    onValue(refArtikel, snap => {
+      const data = snap.val() as ArtikelData
+
+      setTitle(data.title)
+      setContent(data.content)
+      setImageUrl(data.imageUrl)
+    })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div className='bg-gray-50 text-gray-800 relative'>
+      <Navbar />
+
+      <div id='a' className='pt-16' data-aos='fade-up' data-aos-delay='300'>
+        <img src={imageUrl} alt={title} className='w-full h-96 object-cover' />
+        <div className='container mx-auto space-y-8 py-6'>
+          <h1 className='text-4xl font-extrabold tracking-tight text-balance'>{title}</h1>
+          <p className='indent-8 leading-relaxed text-justify'>{content}</p>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
